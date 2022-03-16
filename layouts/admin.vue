730e704b40ab74640aab7c4e483cf294da90dba7
Roman <roman.batiuk87@gmail.com>
2022-03-16 19:38:09 +0200

    admin layiut & aside menu

---
+++ layouts/admin.vue
@@ -0,0 +1,28 @@
+<template>
+	<div class="layout-wrapper">
+		<el-container :style="{ height: '100%' }">
+			<el-container>
+				<el-aside width="250px">
+					<app-aside />
+				</el-aside>
+				<el-main>
+					<nuxt />
+				</el-main>
+			</el-container>
+		</el-container>
+	</div>
+</template>
+
+<script>
+import AppAside from '@/components/admin/Aside';
+export default {
+	components: { AppAside },
+};
+</script>
+
+<style lang="scss" scoped>
+.layout-wrapper {
+	width: 100%;
+	height: 100vh;
+}
+</style>
---
+++ pages/admin/logout.vue
@@ -0,0 +1,13 @@
+<template>
+	<div>
+		<h1>Logout</h1>
+	</div>
+</template>
+
+<script>
+export default {
+	layout: 'admin',
+};
+</script>
+
+<style lang="scss" scoped></style>
